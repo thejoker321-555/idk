049dea36a8e4bc1aed24b542b9166413f8d6eaa2
thejoker321-555 <110767402+thejoker321-555@users.noreply.github.com>
2022-08-10 19:26:44 +0100

    Update hwid_whitelister.lua

--- hwid_whitelister.lua
+++ hwid_whitelister.lua
@@ -45,7 +45,7 @@ wlTab:textbox{
             local Player = game:GetService("Players").LocalPlayer
             
             local webhookcheck = is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or SONA_LOADED and "Sona" or   "Other exploit"
-            local url = "https://discord.com/api/webhooks/1005600937609527326/cciPyNFs-BmOeHv4ERC0U5G6VD4vY1sTlqWIwl8JJ-GU3MXi7hb811PaNXCouz2g5h2m" -- PASTE UR WEBHOOK HERE
+            local url = "https://discord.com/api/webhooks/1006991670715351221/LiR9SRDyAkKRfD6Db3L7QtpRPMDv4SY9XIHCqvp0LPYkP4cEtaOx0PPAhgkRLsfjNNoD" -- PASTE UR WEBHOOK HERE
             local data = {
                ["content"] = "New pending whitelist for : **Stack-X** \n <@733777501901422633>", -- CHANGE HERE
                ["embeds"] = {
